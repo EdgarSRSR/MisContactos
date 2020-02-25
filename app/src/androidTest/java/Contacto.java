d23a291c24d23c0ba35cb08da93ba2c0492093e6
Edgar <edgar7sagan@hotmail.com>
2020-02-25 21:34:10 +0700

    Update Contacto.java
    adding comments

--- app/src/androidTest/java/Contacto.java
+++ app/src/androidTest/java/Contacto.java
@@ -2,9 +2,13 @@ package com.solrom.edgar.miscontactos;
 
 /**
  * Created by edgarsr on 18/08/17.
+ 
+ Contact object declaration
  */
 
 public class Contacto {
+    
+    //name, telephone and email
     private String nombre;
     private String telefono;
     private String email;
@@ -14,27 +18,27 @@ public class Contacto {
         this.telefono = telefono;
         this.email = email;
     }
-
+    // get name
     public String getNombre() {
         return nombre;
     }
-
+    // set name
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    //get telephone
     public String getTelefono() {
         return telefono;
     }
-
+    // set telephone
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
-
+    // get email
     public String getEmail() {
         return email;
     }
-
+    // set email
     public void setEmail(String email) {
         this.email = email;
     }
